1e003b8443241095bdac082632a87956d15753ef
Lyanban <banlyan@qq.com>
2021-10-10 14:16:09 +0800

    为 admin 模块添加分页功能

--- admin/src/main/java/com/lyanba/config/AdminConfig.java
+++ admin/src/main/java/com/lyanba/config/AdminConfig.java
@@ -1,5 +1,9 @@
 package com.lyanba.config;
 
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.pagination.optimize.JsqlParserCountOptimize;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -8,7 +12,7 @@ import org.springframework.web.filter.CorsFilter;
 
 /**
  * @className: AdminConfig
- * @description:
+ * @description: admin 模块的配置类
  * @author: LyanbA
  * @since: 2021/10/10 00:34
  * @todo:
@@ -31,5 +35,27 @@ public class AdminConfig {
         // 第一个参数表示要过滤的地址
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
+    }
+
+    /*
+    // 旧版分页插件
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        // 设置请求的页面大于最大页后操作， true调回到首页，false 继续请求  默认false
+        // paginationInterceptor.setOverflow(false);
+        // 设置最大单页限制数量，默认 500 条，-1 不受限制
+        // paginationInterceptor.setLimit(500);
+        // 开启 count 的 join 优化,只针对部分 left join
+        paginationInterceptor.setCountSqlParser(new JsqlParserCountOptimize(true));
+        return paginationInterceptor;
+    }*/
+
+    // 新版分页插件
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.H2));
+        return interceptor;
     }
 }
--- admin/src/main/java/com/lyanba/controller/UmsAdminController.java
+++ admin/src/main/java/com/lyanba/controller/UmsAdminController.java
@@ -1,6 +1,7 @@
 package com.lyanba.controller;
 
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.lyanba.entry.UmsAdmin;
 import com.lyanba.service.UmsAdminService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * <p>
@@ -25,9 +27,19 @@ public class UmsAdminController {
     @Autowired
     UmsAdminService umsAdminService;
 
-    @GetMapping("/list")
+    /*@GetMapping("/list")
     List<UmsAdmin> list() {
         return umsAdminService.list();
+    }*/
+
+    /*
+    添加分页功能
+     */
+    @GetMapping("/list")
+    Page<UmsAdmin> list(int pageNo, int pageSize) throws InterruptedException {
+        // 模拟网络延时
+        TimeUnit.SECONDS.sleep(1);
+        return umsAdminService.page(new Page<>(pageNo, pageSize));
     }
 }
 
